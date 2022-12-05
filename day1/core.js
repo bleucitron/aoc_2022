c16788787f4313396b13366e78ae24ab8de4a2fa
Romain Crestey <rcrestey@gmail.com>
2022-12-05 18:28:19 +0100

    clean day1

--- day1/core.js
+++ day1/core.js
@@ -2,7 +2,7 @@ export default function core(input) {
   const data = input.trim().split('\n\n');
 
   const loads = data.map(elf => {
-    return elf.split('\n').reduce((a, nb) => a + parseInt(nb || 0), 0);
+    return elf.split('\n').reduce((a, nb) => a + parseInt(nb), 0);
   });
 
   return loads;
