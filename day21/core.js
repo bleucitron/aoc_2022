f196f3aa8440b46ac489231bd48540dfc23e6d90
Romain Crestey <rcrestey@gmail.com>
2022-12-21 17:51:33 +0100

    day 21

--- day21/core.js
+++ day21/core.js
@@ -1,5 +1,69 @@
 export function parse(input) {
-  const data = input.trim().split('\n');
+  return input.trim().split('\n');
+}
 
-  return data;
+export function makeMap1(data) {
+  const m = new Map();
+
+  data.forEach(line => {
+    const [id, op] = line.split(': ');
+
+    const n = parseInt(op);
+    if (n) {
+      m.set(id, n);
+      return;
+    }
+
+    const r = op.match(/(.+) (\+|-|\*|\/) (.+)/);
+
+    m.set(id, () => {
+      // console.log('ID', id);
+      let x = m.get(r[1]);
+      let y = m.get(r[3]);
+
+      x = typeof x === 'number' ? parseInt(x) : x();
+      y = typeof y === 'number' ? parseInt(y) : y();
+
+      // console.log({ x, y });
+
+      return eval(`${x} ${r[2]} ${y}`);
+    });
+  });
+
+  return m;
+}
+
+export function makeMap2(data) {
+  const m = new Map();
+
+  data.forEach(line => {
+    let [id, op] = line.split(': ');
+
+    const n = parseInt(op);
+    if (n) {
+      m.set(id, n);
+      return;
+    }
+
+    const r = op.match(/(.+) (\+|-|\*|\/) (.+)/);
+    const arg1 = r[1];
+    const arg2 = r[3];
+    let operand = r[2];
+
+    if (id === 'root') {
+      operand = operand.replace('+', '-');
+    }
+
+    m.set(id, () => {
+      let x = m.get(arg1);
+      let y = m.get(arg2);
+
+      x = typeof x === 'number' ? parseInt(x) : x();
+      y = typeof y === 'number' ? parseInt(y) : y();
+
+      return eval(`${x} ${operand} ${y}`);
+    });
+  });
+
+  return m;
 }
--- day21/index.js
+++ day21/index.js
@@ -1,13 +1,85 @@
-import { parse } from './core.js';
+import { parse, makeMap1, makeMap2 } from './core.js';
 
 export function part1(input) {
-  const data = parse(input);
+  const map = makeMap1(parse(input));
 
-  return data.length;
+  const root = map.get('root');
+
+  return root();
 }
 
 export function part2(input) {
   const data = parse(input);
 
-  return data.length;
+  const map = makeMap2(data);
+  const root = map.get('root');
+
+  // find slope and offset to determine which way to explore
+  const x1 = 0;
+  const x2 = 10;
+
+  map.set('humn', 0);
+  const offset = root();
+  map.set('humn', x1);
+  const y1 = root();
+  map.set('humn', x2);
+  const y2 = root();
+
+  const slope = (y2 - offset) / x2;
+
+  const positive = (offset > 0 && slope < 0) || (offset < 0 && slope > 0);
+
+  let i = positive ? 1 : -1;
+  let prec;
+  let found = false;
+  let min;
+  let max;
+
+  // find boundaries
+  while (found === false) {
+    map.set('humn', i);
+    const n = root();
+
+    if (n === 0) {
+      found = true;
+      break;
+    }
+
+    if (!prec || Math.sign(prec) === Math.sign(n)) {
+      i *= 2;
+    } else {
+      min = positive ? i / 2 : i;
+      max = positive ? i : i / 2;
+
+      i = min + Math.floor((max - min) / 2);
+      break;
+    }
+
+    prec = n;
+  }
+
+  // dichotomy
+  while (!found) {
+    map.set('humn', i);
+    const n = root();
+
+    if (n === 0) {
+      found = true;
+      break;
+    }
+    map.set('humn', min);
+    const yMin = root();
+
+    map.set('humn', max);
+    const yMax = root();
+
+    if (Math.abs(yMin) < Math.abs(yMax)) {
+      max = i;
+    } else {
+      min = i;
+    }
+    i = min + Math.floor((max - min) / 2);
+  }
+
+  return i;
 }
